2378a3671d5d2e31cf880b4f8b9c82e137371d45
arunparihar <arunsinghparihar3219@gmail.com>
2024-07-03 15:30:06 +0530

    some minor changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = ({ handleOrderPopup }) => {
           <div className="container py-[2px] sm:block hidden">
             <div className="flex items-center justify-between">
               <p className="text-sm">20% off on next booking</p>
-              <p>mobile no. +91 123456789</p>
+              <p>Mobile No. +91 9302481268</p>
             </div>
           </div>
         </div>
